528730b69f8159037a072eb5836b5047996db0c5
Sam Johnson <sjohnson540@gmail.com>
2015-11-13 12:58:39 -0500

    Add half-open handling

--- breaker/breaker.py
+++ breaker/breaker.py
@@ -1,3 +1,4 @@
+import datetime
 
 class CircuitBreaker(object):
     """A circuit breaker which handles requests, timeouts
@@ -9,12 +10,13 @@ class CircuitBreaker(object):
     """
     OPEN = "open"
     CLOSED = "closed"
-    HALF_OPEN = "half_open"
 
-    def __init__(self, func, failures_allowed=10, timeout=1000):
+    def __init__(self, func, failures_allowed=10, timeout=10):
         """ Create a CircuitBreaker that has a default of 10
         failures in a row to be tripped, and a timeout of 1s
         for each request.
+
+        Timeout is measured in seconds
 
         It accepts a function to test as call.
         """
@@ -25,32 +27,42 @@ class CircuitBreaker(object):
         self.func = func
 
     def call(self, *args, **kwargs):
-        if self.state() == self.OPEN:
+        if self.state() == self.OPEN and not self.is_half_open():
             raise CircuitBreakerException("Circuit breaker tripped")
 
         try:
             result = self.func(*args, **kwargs)
             self.reset()
             return result
-        except Exception as e:
+        except Exception as error:
             self.failure()
-            raise e
+            raise error
 
-    def failure(self):
-        """ Increments the failure count and checks to see
-        if the circuit breaker needs tripped."""
-        self.num_failures += 10
+    def state(self):
+        """ Current state of ciruit breaker system """
+        if self.num_failures > self.failures_allowed:
+            return self.OPEN
+        else:
+            return self.CLOSED
+
+    def is_half_open(self):
+        """ Check to see if we should test the connection again """
+        if self.last_failure:
+            now = datetime.datetime.now()
+            difference = now - self.last_failure
+            return difference.seconds > self.timeout
+        return False
 
     def reset(self):
         """ Reset the failure counters because a request was a
         success."""
         self.num_failures = 0
 
-    def state(self):
-        if self.num_failures > self.failures_allowed:
-            return self.OPEN
-        else:
-            return self.CLOSED
+    def failure(self):
+        """ Increments the failure count and checks to see
+        if the circuit breaker needs tripped."""
+        self.num_failures += 1
+        self.last_failure = datetime.datetime.now()
 
 
 class CircuitBreakerException(Exception):
--- breaker/test_breaker.py
+++ breaker/test_breaker.py
@@ -3,8 +3,18 @@ from .breaker import (
     CircuitBreakerException
 )
 
+import datetime
+import time
+
 def func(a, b, c=10):
     return a + b + c
+
+def error_func():
+    l = []
+    return l[10]
+
+def error2_func():
+    raise Exception
 
 def test_call():
     cb = CircuitBreaker(func)
@@ -19,8 +29,9 @@ def test_reset():
 
 def test_failure():
     cb = CircuitBreaker(func)
+    old_failures = cb.num_failures
     cb.failure()
-    assert cb.num_failures > 0
+    assert old_failures + 1 == cb.num_failures
 
 def test_closed_breaker():
     cb = CircuitBreaker(func)
@@ -30,3 +41,51 @@ def test_closed_breaker():
         assert False
     except CircuitBreakerException:
         assert True
+
+def test_error_functions():
+    cb = CircuitBreaker(error_func)
+    for i in range(12):
+        try:
+            cb.call()
+        except CircuitBreakerException:
+            assert i > 10
+        except IndexError:
+            assert i <= 10
+
+def test_not_half_open():
+    cb = CircuitBreaker(func)
+    assert cb.is_half_open() is False
+
+def test_half_open():
+    cb = CircuitBreaker(error_func)
+    cb.num_failures = 20
+    double_ago = past(2 * cb.timeout)
+    cb.last_failure = double_ago
+    assert cb.is_half_open() is True
+
+def test_half_open_close():
+    cb = CircuitBreaker(error_func, 1, .5)
+    for i in range(2):
+        try:
+            cb.call()
+            assert False
+        except Exception:
+            assert True
+
+    time.sleep(1)
+    assert cb.is_half_open() is True
+
+    # Should try the call again if its half open
+    try:
+        cb.call()
+        assert False
+    except IndexError:
+        assert True
+    except CircuitBreakerException:
+        assert False
+
+    assert cb.state() == cb.OPEN
+    assert cb.is_half_open() is False
+
+def past(seconds):
+    return datetime.datetime.now() - datetime.timedelta(seconds=seconds)
